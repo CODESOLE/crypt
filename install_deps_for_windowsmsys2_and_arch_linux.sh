0904ab0e67db39bba7bd4025dc9be8bc6de04f46
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2022-03-21 14:05:34 +0300

    install script update

--- install_deps_for_windowsmsys2_and_arch_linux.sh
+++ install_deps_for_windowsmsys2_and_arch_linux.sh
@@ -3,9 +3,11 @@
 if [ -z "${MSYSTEM}" ] && [ "$(uname)" = "Linux" ] && [ -x "$(command -v pacman)" ]; then
   echo LINUX_PACMAN
   pacman -S --needed --noconfirm base-devel premake ncurses
-elif [ "${MSYSTEM}" = "MSYS" ]; then
+elif [ -n "${MSYSTEM}" ] && [ "${MSYSTEM}" = "MSYS" ]; then
   echo MSYS2_MINGW64
   pacman -S --needed --noconfirm base-devel mingw-w64-x86_64-{toolchain,premake,ncurses}
+elif [ -n "${MSYSTEM}" ] && [ "${MSYSTEM}" != "MSYS" ]; then
+  echo "You executed this in MSYS2 ${MSYSTEM} shell which is invalid MSYS2 shell. It should be executed in MSYS2 MSYS shell."
 else
   echo "Unknown environment, this script file should be executed either in Linux with pacman package manager or in MSYS2_MSYS shell (NOT MSYS MINGW shell) on Windows."
 fi
