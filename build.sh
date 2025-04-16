f388220b6feb8817c5b85a2f60d58038401a6232
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2025-04-16 17:19:25 +0300

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ "$1" = "r" ]; then
-  echo ==================RELEASE================== && mkdir -p build
-  premake5 gmake2 && cd build/ && make config=release_x64
-else
-  echo ===================DEBUG=================== && mkdir -p build
-  premake5 gmake2 && cd build/ && make config=debug_x64
-fi
